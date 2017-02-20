eb16e2dbe481f8f6030417d74d80413c10e5c552
Майстренко Екатерина Алексеевна <ip612s16@awp11.csc.local>
2017-02-20 09:17:35 +0700

    first commit- vvodnai chst'

---
+++ MAIN.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int n;
+	int k;
+  printf("Vvedite summu vklada");
+  scanf("%d", &k);
+  printf("Vvedite comandu");
+  scanf("%d", &n);
+  
+  
+
+return 0;
+}
